3165d92d301945db9aa200fae8ff208c7bad272e
Marcin Mika <marcinmika@hotmail.com>
2017-06-11 14:52:04 +0200

    zadanie 1 z pdf

--- js/warsztat.js
+++ js/warsztat.js
@@ -1,34 +1,75 @@
-
+//
+//    'use strict';
+//
+//
+//var objekt = {"pracownicy":[
+//{"firstName": "Krystian", "lastName": "Dziopa"},
+//{"firstName": "Anna", "lastName": "Szapiel"},
+//{"firstName": "Piotr", "lastName": "Żmuda"}
+//]
+//};
+//
+////console.log(objekt);
+//
+//
+////var obj = JSON.parse(objekt);
+//
+//
+//function iteracjaPracownikow()
+//
+//{
     'use strict';
 
-
-var objekt = {"pracownicy":[
-{"firstName": "Krystian", "lastName": "Dziopa"},
-{"firstName": "Anna", "lastName": "Szapiel"},
-{"firstName": "Piotr", "lastName": "Żmuda"}
-]
-};
-
-//console.log(objekt);
-
-
-//var obj = JSON.parse(objekt);
-
-
-function iteracjaPracownikow()
-
-{
-    for (var i=0; i<objekt.pracownicy.length; i++) {
-        console.log(objekt.pracownicy[i].firstName);
-    }
+//    'use strict';
+//    for (var i=0; i<objekt.pracownicy.length; i++) {
+//        console.log(objekt.pracownicy[i].firstName);
+//    }
+//}
+//
+//iteracjaPracownikow();
+////**********************************
+//
+//jsonPracownicy.pracownicy.forEach( function (el, i) {
+//    console.log("Pracownik " + i + " :" + el.firstName + "  " + el.lastName);
+//});
+//*****************************************
+/*zadanie z pdf materialy-dom zad 1*/
+function klik(){
+    document.getElementsByTagName('p')[0].style.backgroundColor='red';
+    document.getElementsByTagName('p')[1].style.backgroundColor='red';
+    
+    
+    console.log('kliknolem');
 }
 
-iteracjaPracownikow();
-//**********************************
+document.getElementsByTagName('button')[0].onclick=klik;
 
-jsonPracownicy.pracownicy.forEach( function (el, i) {
-    console.log("Pracownik " + i + " :" + el.firstName + "  " + el.lastName);
-});
+//*********************************
+//ZADANIE 2 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 //to samo tylko w foreach
